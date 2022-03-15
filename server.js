c508b1f0eb713fa7cf03f852b4c2db955b99f7b1
Diego Kaku <diegokaku@gmail.com>
2022-03-14 23:51:37 -0500

    working now... no clue ho

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes/api');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -12,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
